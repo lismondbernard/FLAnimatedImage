e3182508546024bf0ca9bf2048e2b9aa2ce8726a
Lismond Bernard <lismond@acst.com>
2020-08-24 16:37:19 -0400

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         .target(
             name: "FLAnimatedImage",
             dependencies: [],
-            path: "FLAnimatedImage"),
+            path: "FLAnimatedImage",
+            publicHeadersPath: "FLAnimatedImage"),
     ]
 )
